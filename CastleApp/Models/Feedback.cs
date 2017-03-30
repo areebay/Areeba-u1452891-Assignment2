9bd01032cfc24fe2563811a39ffb12580bc540af
Areeba <areeba.y@hotmail.co.uk>
2017-03-30 10:32:04 +0100

    Add project files.

---
+++ CastleApp/Models/Feedback.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcFeedback.Models
+{
+    public class Feedback
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Client Name")]
+        public string ClientName { get; set; }
+
+        public string Comment { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public int Rating { get; set; }
+
+        public Feedback()
+        {
+            Date = DateTime.Now;
+        }
+    }
+
+    public class CastleDBContext : DbContext
+    {
+        public DbSet<Feedback> Feedback { get; set; }
+    }
+}
---
+++ CastleApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CastleApp.Startup))]
+namespace CastleApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
